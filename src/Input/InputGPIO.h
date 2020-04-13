140b3d9879dfff5648c6dfb208e961bafded5522
Emil Gajšak <emil.gajsak@gmail.com>
2020-04-13 15:24:20 +0200

    Added separate GPIO and I2C input types (#1)
    * Added GPIO and I2C input types
    
    * Tidied up Input child classes
    
    * Moved I2C and GPIO related functions from Input
    
    * Move Input debouncing to concrete Input classes
    
    * Use InputGPIO in CircuitOS.ino
    
    * Use InputGPIO in ListMenu example
    
    * Added local keypad and keypad_i2ca libraries
    
    * Moved Keypad back as an external dep
    
    Co-authored-by: Filip Budiša <filip.budisa@gmail.com>
    Co-authored-by: Filip Budiša <filip@circuitmess.com>

---
+++ src/Input/InputGPIO.h
@@ -0,0 +1,21 @@
+#ifndef CIRCUITOS_INPUTGPIO_H
+#define CIRCUITOS_INPUTGPIO_H
+
+#include "Input.h"
+#include "../Util/Vector.h"
+#include "../Util/Task.h"
+
+#define PIN_MAX 45
+#define DEBOUNCE_COUNT 1
+
+class InputGPIO : public Input {
+public:
+	InputGPIO();
+
+private:
+	void registerButton(uint8_t pin) override;
+	void scanButtons() override;
+};
+
+
+#endif //CIRCUITOS_INPUTGPIO_H
