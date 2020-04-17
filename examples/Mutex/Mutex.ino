1b6a3f222b8c725bca0adda6b1005d94e23ef96e
Filip Budiša <filip@circuitmess.com>
2020-04-17 19:44:41 +0200

    Change Mutex example formatting

--- examples/Mutex/Mutex.ino
+++ examples/Mutex/Mutex.ino
@@ -16,22 +16,30 @@ void setup(){
 
 void loop(){
 	Serial.println("Task 1 trying to enter critical section");
-	mutex.lock();
+
+	mutex.lock(); // -- critical section begins
+
 	Serial.println("Task 1 entered");
 	usleep(1000 * (2000 + rand() % 1000));
 	Serial.println("Task 1 exiting critical section");
-	mutex.unlock();
+
+	mutex.unlock(); // - critical section end
+
 	usleep(1000 * (2000 + rand() % 1000));
 }
 
 void loop_task(Task* task){
 	while(task->running){
 		Serial.println("Task 2 trying to enter critical section");
-		mutex.lock();
+
+		mutex.lock(); // -- critical section begins
+
 		Serial.println("Task 2 entered");
 		usleep(1000 * (2000 + rand() % 1000));
 		Serial.println("Task 2 exiting critical section");
-		mutex.unlock();
+
+		mutex.unlock(); // - critical section end
+
 		usleep(1000 * (2000 + rand() % 1000));
 	}
 }
