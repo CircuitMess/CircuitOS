78c711f4d44a509f47a96b64f733c6c39fcfce44
Emil Gajšak <emil.gajsak@gmail.com>
2020-04-19 18:09:50 +0200

    added i2cMutex

--- src/Input/InputI2C.cpp
+++ src/Input/InputI2C.cpp
@@ -1,7 +1,7 @@
 #include "InputI2C.h"
 #include "../Util/Debug.h"
 
-InputI2C::InputI2C(I2cExpander* _i2c) : Input(I2C_PIN_MAX) , i2c(_i2c){
+InputI2C::InputI2C(I2cExpander* _i2c, Mutex* _i2cMutex) : Input(I2C_PIN_MAX) , i2c(_i2c), mutex(_i2cMutex){
 
 }
 
@@ -11,19 +11,87 @@ void InputI2C::start(){
 }
 
 void InputI2C::scanButtons(){
-	int portScan = i2c->portRead();
-	// Serial.println(portScan, BIN);
+	if(mutex != nullptr)
+	{
+		Serial.println("before btn crit");
+		delay(5);
+		if(mutex->lock())
+		{
+			Serial.println("btn crit");
+			delay(5);
+			int portScan = i2c->portRead();
+			mutex->unlock();
+			Serial.println("after btn crit");
+			delay(5);
+			// Serial.println(portScan, BIN);
 
-	for(uint i = 0; i < buttons.size(); i++){
-		if(!bitRead(portScan, buttons[i])){
-			Input::btnPress(i);
-		}else{
-			Input::btnRelease(i);
+			for(uint i = 0; i < buttons.size(); i++){
+				if(!bitRead(portScan, buttons[i])){
+					Input::btnPress(i);
+				}else{
+					Input::btnRelease(i);
+				}
+			}
 		}
 	}
+// entering critical
+// exiting critical
+// before btn crit
+// btn crit
+// after btn crit
+// before btn crit
+// btn crit
+// after btn crit
+// before btn crit
+// btn crit
+// after btn crit
+// before btn crit
+// btn crit
+// after btn crit
+// before btn crit
+// btn crit
+// after btn crit
+// before btn crit
+// btn crit
+// after btn crit
+// before btn crit
+// btn crit
+// after btn crit
+// before btn crit
+// before critical
+// btn crit
+// entering critical
+// after btn crit
+// toggel
+// exiting critical
+	else
+	{
+		int portScan = i2c->portRead();
+		for(uint i = 0; i < buttons.size(); i++){
+			if(!bitRead(portScan, buttons[i])){
+				Input::btnPress(i);
+			}else{
+				Input::btnRelease(i);
+			}
+		}
+	}
+	
 }
 
 void InputI2C::registerButton(uint8_t pin){
-	i2c->pinMode(pin, INPUT_PULLUP);
-	Input::registerButton(pin);
+	if(mutex != nullptr)
+	{
+		if(mutex->lock())
+		{
+			i2c->pinMode(pin, INPUT_PULLUP);
+			mutex->unlock();
+			Input::registerButton(pin);
+		}
+	}
+	else
+	{
+		i2c->pinMode(pin, INPUT_PULLUP);
+		Input::registerButton(pin);
+	}
+	
 }
--- src/Sync/Mutex.h
+++ src/Sync/Mutex.h
@@ -1,6 +1,6 @@
 #ifndef CIRCUITOS_MUTEX_H
 #define CIRCUITOS_MUTEX_H
-
+#define INCLUDE_vTaskSuspend 1
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
 
@@ -9,7 +9,7 @@ public:
 	Mutex();
 	~Mutex();
 
-	void lock();
+	bool lock();
 	void unlock();
 
 private:
