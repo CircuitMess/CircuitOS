1a24818231bbaca1b2de9c26ab40bbf04b999a01
Emil Gajšak <emil.gajsak@gmail.com>
2020-04-23 16:44:51 +0200

    InputI2c mutex (#9)
    * added i2cMutex
    
    * Added bool return in semaphore, simplifed i2cMutex
    
    * Fixed BinarySemaphore::wait not returning bool
    
    * Moving mutex changes to separate branch

--- src/Input/InputI2C.cpp
+++ src/Input/InputI2C.cpp
@@ -1,7 +1,7 @@
 #include "InputI2C.h"
 #include "../Util/Debug.h"
 
-InputI2C::InputI2C(I2cExpander* _i2c) : Input(I2C_PIN_MAX) , i2c(_i2c){
+InputI2C::InputI2C(I2cExpander* _i2c, Mutex* _i2cMutex) : Input(I2C_PIN_MAX) , i2c(_i2c), mutex(_i2cMutex){
 
 }
 
@@ -11,9 +11,11 @@ void InputI2C::start(){
 }
 
 void InputI2C::scanButtons(){
+	if(mutex != nullptr)
+		mutex->lock();
 	int portScan = i2c->portRead();
-	// Serial.println(portScan, BIN);
-
+	if(mutex != nullptr)
+		mutex->unlock();
 	for(uint i = 0; i < buttons.size(); i++){
 		if(!bitRead(portScan, buttons[i])){
 			Input::btnPress(i);
@@ -24,6 +26,10 @@ void InputI2C::scanButtons(){
 }
 
 void InputI2C::registerButton(uint8_t pin){
+	if(mutex != nullptr)
+		mutex->lock();
 	i2c->pinMode(pin, INPUT_PULLUP);
+	if(mutex != nullptr)
+		mutex->unlock();
 	Input::registerButton(pin);
 }
--- src/Input/InputI2C.h
+++ src/Input/InputI2C.h
@@ -4,7 +4,7 @@
 #include "Input.h"
 #include "../Util/Vector.h"
 #include "../Util/Task.h"
-
+#include "../Sync/Mutex.h"
 #include <Wire.h>
 #include "I2cExpander.h"
 
@@ -13,7 +13,7 @@
 
 class InputI2C : public Input {
 public:
-	InputI2C(I2cExpander* _i2c);
+	InputI2C(I2cExpander* _i2c, Mutex* _i2cMutex = nullptr);
 
 	void start() override;
 	
@@ -21,7 +21,7 @@ private:
 	I2cExpander* i2c;
 	void registerButton(uint8_t pin) override;
 	void scanButtons() override;
-
+	Mutex* mutex;
 };
 
 
