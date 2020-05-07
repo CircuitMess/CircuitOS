9ecc8e48409e497478e088a56a284de6c6869684
Filip Budiša <filip@circuitmess.com>
2020-05-07 16:08:54 +0200

    Add Queue and accompanying example

---
+++ src/Sync/Queue.cpp
@@ -0,0 +1,44 @@
+#include "Queue.h"
+
+Queue::Queue(size_t queueSize, size_t msgSize){
+	queue = xQueueCreate(queueSize, msgSize);
+}
+
+Queue::~Queue(){
+	vQueueDelete(queue);
+}
+
+int Queue::count(){
+	return uxQueueMessagesWaiting(queue);
+}
+
+void Queue::reset(){
+	xQueueReset(queue);
+}
+
+bool Queue::peek(void* buffer){
+	return xQueuePeek(queue, buffer, 0);
+}
+
+bool Queue::send(void* msg){
+	return xQueueSend(queue, msg, portMAX_DELAY);
+}
+
+bool Queue::receive(void* msg){
+	return xQueueReceive(queue, msg, portMAX_DELAY);
+}
+
+void Queue::resize(size_t queueSize){
+	vQueueDelete(queue);
+	queue = xQueueCreate(queueSize, msgSize);
+	this->queueSize = queueSize;
+}
+
+size_t Queue::getQueueSize() const{
+	return queueSize;
+}
+
+size_t Queue::getMsgSize() const{
+	return msgSize;
+}
+
