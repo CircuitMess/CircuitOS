aa522eb93a6663262ac3e7139b01069bb683f804
Filip Budiša <filip@circuitmess.com>
2020-06-24 16:27:48 +0200

    Remove mutex from InputI2C

--- src/Input/InputI2C.cpp
+++ src/Input/InputI2C.cpp
@@ -1,21 +1,12 @@
 #include "InputI2C.h"
-#include "../Util/Debug.h"
+#include "I2cExpander.h"
 
-InputI2C::InputI2C(I2cExpander* _i2c, Mutex* _i2cMutex) : Input(I2C_PIN_MAX) , i2c(_i2c), mutex(_i2cMutex){
+InputI2C::InputI2C(I2cExpander* _i2c) : Input(I2C_PIN_MAX) , i2c(_i2c){
 
-}
-
-void InputI2C::start(){
-	Input::start();
-	logln("Started I2C input");
 }
 
 void InputI2C::scanButtons(){
-	if(mutex != nullptr)
-		mutex->lock();
 	int portScan = i2c->portRead();
-	if(mutex != nullptr)
-		mutex->unlock();
 	for(uint i = 0; i < buttons.size(); i++){
 		if(bitRead(portScan, buttons[i])){
 			Input::btnRelease(i);
@@ -28,10 +19,6 @@ void InputI2C::scanButtons(){
 }
 
 void InputI2C::registerButton(uint8_t pin){
-	if(mutex != nullptr)
-		mutex->lock();
 	i2c->pinMode(pin, INPUT_PULLUP);
-	if(mutex != nullptr)
-		mutex->unlock();
 	Input::registerButton(pin);
 }
