bae93f2bd303858bade9fd181269af0c211670db
Emil Gajšak <emil.gajsak@gmail.com>
2020-07-26 18:48:34 +0200

    Changed context transitions to prevent RAM usage

--- src/Support/Context.cpp
+++ src/Support/Context.cpp
@@ -35,7 +35,11 @@ void Context::unpack(){
 
 void Context::pop(){
 	if(parent == nullptr) return;
-	new ContextTransition(*screen.getDisplay(), this, parent, true);
+	this->pack();
+	this->stop();
+	parent->unpack();
+	parent->start();
+	// new ContextTransition(*screen.getDisplay(), this, parent, true);
 	parent = nullptr;
 }
 
@@ -47,7 +51,11 @@ void Context::pop(void* data){
 
 void Context::push(Context* parent){
 	this->parent = parent;
-	new ContextTransition(*screen.getDisplay(), parent, this);
+	// new ContextTransition(*screen.getDisplay(), parent, this);
+	parent->stop();
+	parent->pack();
+	this->unpack();
+	// this->start();
 }
 
 void Context::returned(void* data){
