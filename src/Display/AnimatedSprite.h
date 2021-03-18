baf1de84e23d7f9e4a142be84eef551dd7da1d08
Filip Budiša <filip@circuitmess.com>
2021-03-18 21:54:00 +0100

    Reworked AnimatedSprite to use GIF565 format

--- src/Display/AnimatedSprite.h
+++ src/Display/AnimatedSprite.h
@@ -5,49 +5,81 @@
 #include <vector>
 #include "../Loop/LoopListener.h"
 #include "Sprite.h"
+#include "../UI/CustomElement.h"
+#include "../UI/SpriteElement.h"
 #include <FS.h>
-
-class gd_GIF;
 
 class AnimatedSprite {
 public:
-	AnimatedSprite(Sprite* parentSprite, fs::File gifFile);
+	AnimatedSprite(Sprite* canvas, fs::File file, bool loop = false);
 	virtual ~AnimatedSprite();
-
-	int getX() const;
-	void setX(int x);
-	int getY() const;
-	void setY(int y);
-	void setXY(int x, int y);
 
 	void push();
 	void reset();
 	void setLoopDoneCallback(void (*callback)());
-	bool newFrameReady();
 
-private:
-	Sprite* parentSprite = nullptr;
-	int x = 0, y = 0;
-	uint16_t width, height;
-
-	gd_GIF* gif;
-	fs::File gifFile;
-	uint32_t currentFrameTime = 0;
-
-	struct Frame {
-		uint8_t* data = nullptr;
-		uint32_t duration = 0;
-	};
-	Frame currentFrame;
+	/**
+	 * Checks if it's time for a new frame, and if so, loads it. This will fire the LoopDone callback.
+	 * The callback will be called after last frame is over.
+	 * @return True of a new frame is ready, false otherwise.
+	 */
+	bool checkFrame();
 
 	/**
 	 * @brief Function to load the next frame of the GIF, if available.
-	 * 
+	 *
 	 * @return true New frame is loaded successfully.
 	 * @return false Reached the end of GIF, no more frames available.
 	 */
 	bool nextFrame();
 
+	void setX(int16_t x);
+	void setY(int16_t y);
+	void setXY(int16_t x, int16_t y);
+
+	uint16_t getWidth() const;
+	uint16_t getHeight() const;
+
+private:
+	Sprite* canvas;
+	fs::File file;
+	bool loop;
+	size_t dataStart;
+	uint32_t currentFrameTime = 0;
+
+	int16_t x = 0;
+	int16_t y = 0;
+	uint16_t width;
+	uint16_t height;
+
+	struct Header {
+		uint16_t width;
+		uint16_t height;
+		uint16_t noFrames;
+		uint8_t flags;
+	} __attribute__((packed));
+
+	class Table {
+	public:
+		explicit Table(fs::File& file);
+		~Table();
+
+		Color getColor(uint8_t i) const;
+	private:
+		uint8_t noColors;
+		Color* colors;
+	} *table = nullptr;
+
+	struct Frame {
+		uint8_t* data = nullptr;
+		uint16_t duration = 0;
+	} gifFrame;
+
+	uint16_t noFrames = 0;
+	uint16_t currentFrame = 0;
+	uint8_t flags;
+
+	bool done = false;
 	void (*loopDoneCallback)() = nullptr;
 	bool alerted = false;
 };
