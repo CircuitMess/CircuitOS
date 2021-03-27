4f8050b10abd556f343290d49d0c4868fec8954d
Filip Budiša <filip@circuitmess.com>
2021-03-27 21:33:19 +0100

    Add RingBuffer and LazyDataBuffer

---
+++ src/Buffer/LazyDataBuffer.cpp
@@ -0,0 +1,62 @@
+#include "LazyDataBuffer.h"
+
+LazyDataBuffer::LazyDataBuffer(const size_t size) : size(size){
+	if(psramFound()){
+		buffer = static_cast<uint8_t*>(ps_malloc(size));
+	}else{
+		buffer = static_cast<uint8_t*>(malloc(size));
+	}
+}
+
+LazyDataBuffer::~LazyDataBuffer(){
+	free(buffer);
+}
+
+size_t LazyDataBuffer::readAvailable(){
+	return writeCursor - readCursor;
+}
+
+bool LazyDataBuffer::readMove(size_t amount){
+	if(readCursor + amount > writeCursor) return false;
+	readCursor += amount;
+	return true;
+}
+
+uint8_t* LazyDataBuffer::writeData(){
+	return buffer + writeCursor;
+}
+
+void LazyDataBuffer::relocate(){
+	size_t left = writeCursor - readCursor;
+	if(left == 0){
+		writeCursor = readCursor = 0;
+		return;
+	}
+
+	memmove(buffer, buffer + readCursor, left);
+
+	readCursor = 0;
+	writeCursor = left;
+}
+
+void LazyDataBuffer::clear(){
+	readCursor = 0;
+}
+
+const uint8_t* LazyDataBuffer::readData(){
+	return buffer + readCursor;
+}
+
+size_t LazyDataBuffer::writeAvailable(){
+	return size - writeCursor;
+}
+
+size_t LazyDataBuffer::potentialWriteAvailable(){
+	return readCursor;
+}
+
+bool LazyDataBuffer::writeMove(size_t amount){
+	if(writeCursor + amount > size) return false;
+	writeCursor += amount;
+	return true;
+}
---
+++ src/Buffer/RingBuffer.cpp
@@ -0,0 +1,63 @@
+#include "RingBuffer.h"
+
+RingBuffer::RingBuffer(size_t size) : size(size + 1){
+	if(psramFound()){
+		buffer = static_cast<uint8_t*>(ps_malloc(size + 1));
+	}else{
+		buffer = static_cast<uint8_t*>(malloc(size + 1));
+	}
+
+	if(buffer == nullptr){
+		Serial.println("RingBuffer malloc error");
+	}
+}
+
+RingBuffer::~RingBuffer(){
+	free(buffer);
+}
+
+size_t RingBuffer::writeAvailable(){
+	if(end >= beginning) return size + beginning - end - 1;
+	else return beginning - end - 1;
+}
+
+size_t RingBuffer::readAvailable(){
+	if(end >= beginning) return end - beginning;
+	else return size + end - beginning;
+}
+
+size_t RingBuffer::read(uint8_t* destination, size_t n){
+	n = min(n, readAvailable());
+	if(n == 0) return 0;
+
+	if(end > beginning){
+		memcpy(destination, buffer + beginning, n);
+	}else{
+		size_t first = min(size - beginning, n);
+		memcpy(destination, buffer + beginning, first);
+		memcpy(destination + first, buffer, n - first);
+	}
+
+	beginning = (beginning + n) % size;
+	return n;
+}
+
+size_t RingBuffer::write(uint8_t* source, size_t n){
+	n = min(n, writeAvailable());
+	if(n == 0) return 0;
+
+	if(beginning > end){
+		memcpy(buffer + end, source, n);
+	}else{
+		size_t first = min(size - end, n);
+		memcpy(buffer + end, source, first);
+		memcpy(buffer, source + first, n - first);
+	}
+
+	end = (end + n) % size;
+	return n;
+}
+
+void RingBuffer::clear(){
+	beginning = end = 0;
+}
