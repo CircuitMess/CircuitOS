e8377366e993e39e55c3ff0be274549947daf9c7
Emil Gajšak <emil.gajsak@gmail.com>
2021-04-09 17:39:28 +0200

    Added psram allocation in AnimatedSprite

--- src/Display/AnimatedSprite.cpp
+++ src/Display/AnimatedSprite.cpp
@@ -22,9 +22,11 @@ AnimatedSprite::AnimatedSprite(Sprite* canvas, fs::File file) : canvas(canvas), 
 		table = new Table(file);
 		// Serial.printf("Colors: %d\n", table->getNoColors());
 	}
-
-	gifFrame.data = static_cast<uint8_t*>(malloc(width * height * (flags ? 1 : 2)));
-
+	if(psramFound()){
+		gifFrame.data = static_cast<uint8_t*>(ps_malloc(width * height * (flags ? 1 : 2)));
+	}else {
+		gifFrame.data = static_cast<uint8_t *>(malloc(width * height * (flags ? 1 : 2)));
+	}
 	// Serial.printf("Data start: %lu\n", file.position());
 
 	dataStart = file.position();
