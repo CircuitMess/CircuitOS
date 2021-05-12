609c3aba33881d2558261246e9a47ee37ae0e9e7
Emil Gajšak <emil.gajsak@gmail.com>
2021-05-12 09:33:27 +0200

    Added current Context and transitionState (#84)
    * Added current Context and transitionState
    
    * Refactored ContextTransition::isRunning

--- src/Support/ContextTransition.cpp
+++ src/Support/ContextTransition.cpp
@@ -1,5 +1,11 @@
 #include "../../Setup.hpp"
+#include "ContextTransition.h"
 
+bool ContextTransition::transitionRunning = false;
+
+bool ContextTransition::isRunning(){
+	return transitionRunning;
+}
 #ifdef CIRCUITOS_LOWRAM
 	#include "LowRamContextTransition.impl"
 #else
