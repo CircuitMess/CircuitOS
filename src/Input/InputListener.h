b21e63535ca559d3068074987aee5dee5cb68bb8
Emil Gajšak <emil.gajsak@gmail.com>
2021-07-07 15:27:08 +0200

    Added buttonHold and HoldRepeat in InputListener (#89)

--- src/Input/InputListener.h
+++ src/Input/InputListener.h
@@ -2,11 +2,25 @@
 #define CIRCUITOS_INPUTLISTENER_H
 
 #include <Arduino.h>
+#include <map>
 
 class Input;
 
+struct HoldData {
+	uint32_t time;
+	bool holdingOver;
+};
+struct HoldRepeatData {
+	uint32_t time;
+	uint32_t repeatCounter;
+};
+
 class InputListener {
 	friend Input;
+
+protected:
+	void setButtonHoldTime(uint8_t button, uint32_t holdTime);
+	void setButtonHoldAndRepeatTime(uint8_t button, uint32_t holdAndRepeatTime);
 
 private:
 	virtual void buttonPressed(uint i);
@@ -14,6 +28,9 @@ private:
 	virtual void buttonHeld(uint i);
 	virtual void buttonHeldRepeat(uint i, uint repeatCount);
 	virtual void anyKeyCallback();
+
+	std::map<uint8_t, HoldData> holdTimes;
+	std::map<uint8_t, HoldRepeatData> holdAndRepeatTimes;
 };
 
 
