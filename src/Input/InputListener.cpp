b336d9cfee1cfc8255028dfea0a752506853430c
Emil Gajšak <emil.gajsak@gmail.com>
2021-07-07 07:40:52 +0200

    Added buttonHold and HoldRepeat in InputListener

--- src/Input/InputListener.cpp
+++ src/Input/InputListener.cpp
@@ -5,3 +5,12 @@ void InputListener::buttonReleased(uint i) {}
 void InputListener::buttonHeld(uint i) {}
 void InputListener::buttonHeldRepeat(uint i, uint repeatCount) {}
 void InputListener::anyKeyCallback() {}
+
+void InputListener::setButtonHoldAndRepeatTime(uint8_t button, uint32_t holdAndRepeatTime){
+	holdAndRepeatTimes.insert({ button, {holdAndRepeatTime, 0} });
+}
+
+void InputListener::setButtonHoldTime(uint8_t button, uint32_t holdTime){
+	holdTimes.insert({button, {holdTime, false}});
+
+}
