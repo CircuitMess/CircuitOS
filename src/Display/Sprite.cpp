5a617a73b58de8298c486931d375b5a17023696f
Emil Gajšak <emil.gajsak@gmail.com>
2021-08-19 10:27:37 +0200

    Fix #95

--- src/Display/Sprite.cpp
+++ src/Display/Sprite.cpp
@@ -267,7 +267,7 @@ void Sprite::pushImage(int32_t x, int32_t y, int32_t w, int32_t h, uint16_t *dat
 				continue;
 			}
 
-			if(!_iswapBytes) color = color << 8 | color >> 8;
+			if(!getSwapBytes()) color = color << 8 | color >> 8;
 			_img[x + ys * _iwidth] = color;
 			x++;
 		}
