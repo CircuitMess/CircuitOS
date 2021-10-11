5663619329351b8f688d7400208b440d56350628
Filip Budiša <filip@circuitmess.com>
2021-10-11 20:36:10 +0200

    Add I2cExpander::portRead override with error checking

--- src/Input/I2cExpander.h
+++ src/Input/I2cExpander.h
@@ -15,6 +15,7 @@ public:
 	~I2cExpander();
 	void begin(uint8_t _address = 0x74, uint8_t _sda = 27, uint8_t _scl = 14);
 	uint16_t portRead();
+	bool portRead(uint16_t& state);
 	void pinMode(uint8_t pin, uint8_t mode);
 	bool pinRead(uint8_t pin);
 	void pinWrite(uint8_t pin, bool state);
