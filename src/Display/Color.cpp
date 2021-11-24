542cb7a8e0399916f261f98d52ae981fd2f30164
Emil Gajšak <emil.gajsak@gmail.com>
2021-11-24 17:02:06 +0100

    Added HSV color, conversion functions

--- src/Display/Color.cpp
+++ src/Display/Color.cpp
@@ -11,3 +11,68 @@ Color C_HEX(uint32_t hex){
 			hex & 0xFF
 	);
 }
+
+uint32_t HSVtoRGB888(HSV hsv){
+	double      hh, p, q, t, ff;
+	long        i;
+	uint32_t out;
+	double r, g, b;
+
+	if(hsv.s <= 0.0) {       // < is bogus, just shuts up warnings
+		r = hsv.v;
+		g = hsv.v;
+		b = hsv.v;
+	}else{
+		hh = hsv.h;
+		if(hh >= 360.0) hh = 0.0;
+		hh /= 60.0;
+		i = (long)hh;
+		ff = hh - i;
+		p = hsv.v * (1.0 - hsv.s);
+		q = hsv.v * (1.0 - (hsv.s * ff));
+		t = hsv.v * (1.0 - (hsv.s * (1.0 - ff)));
+
+		switch(i){
+			case 0:
+				r = hsv.v;
+				g = t;
+				b = p;
+				break;
+			case 1:
+				r = q;
+				g = hsv.v;
+				b = p;
+				break;
+			case 2:
+				r = p;
+				g = hsv.v;
+				b = t;
+				break;
+
+			case 3:
+				r = p;
+				g = q;
+				b = hsv.v;
+				break;
+			case 4:
+				r = t;
+				g = p;
+				b = hsv.v;
+				break;
+			case 5:
+			default:
+				r = hsv.v;
+				g = p;
+				b = q;
+				break;
+		}
+	}
+	out = (uint32_t)(255.0 * r) | (((uint32_t)(255.0 * g)) << 8) | (((uint32_t)(255.0 * b)) << 16);
+
+	return out;
+}
+
+Color HSVtoRGB565(HSV hsv){
+	uint32_t out = HSVtoRGB888(hsv);
+	return C_RGB(out & 0xFF, out & (0xFF << 8), out & (0xFF << 16));
+}
--- src/Display/Color.h
+++ src/Display/Color.h
@@ -9,4 +9,13 @@ Color C_RGB(uint8_t r, uint8_t g, uint8_t b);
 
 Color C_HEX(uint32_t hex);
 
+struct HSV{
+	uint8_t h; //Hue (0-180)
+	float s; //Saturation (0-1.0)
+	float v; //Value (0-1.0)
+};
+
+uint32_t HSVtoRGB888(HSV hsv);
+Color HSVtoRGB565(HSV hsv);
+
 #endif //CIRCUITOS_COLOR_H
