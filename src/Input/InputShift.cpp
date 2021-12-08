457061afda9c99ebecfaa4471e61a4f1198036b5
hrvojekolar <hrvojekolar1998@gmail.com>
2021-12-08 14:17:25 +0100

    Add input with shift registers InputShift (#114)
    Co-authored-by: Filip Budiša <filip@circuitmess.com>

---
+++ src/Input/InputShift.cpp
@@ -0,0 +1,39 @@
+#include "InputShift.h"
+
+#define PERIOD 1
+#define LH(pin) do { digitalWrite((pin), LOW); delayMicroseconds(PERIOD); digitalWrite((pin), HIGH); delayMicroseconds(PERIOD); } while(0)
+#define HL(pin) do { digitalWrite((pin), HIGH); delayMicroseconds(PERIOD); digitalWrite((pin), LOW); delayMicroseconds(PERIOD); } while(0)
+
+InputShift::InputShift(uint8_t dataPin, uint8_t clockPin, uint8_t loadPin, uint8_t numButtons) : dataPin(dataPin), clockPin(clockPin), loadPin(loadPin),
+numButtons(numButtons), numShifts(ceil((float) numButtons / 8.0f)), Input(numButtons){
+
+	for(int i = 0; i < numButtons; i++){
+		InputShift::registerButton(i);
+	}
+}
+
+void InputShift::begin(){
+	pinMode(dataPin, INPUT);
+	pinMode(clockPin, OUTPUT);
+	pinMode(loadPin, OUTPUT);
+}
+
+void InputShift::scanButtons(){
+	digitalWrite(clockPin, LOW);
+	LH(loadPin);
+
+	std::vector<bool> states(numShifts * 8, true);
+	for(int i = 0; i < numShifts * 8; i++){
+		states[i] = digitalRead(dataPin) == HIGH;
+		HL(clockPin);
+	}
+
+	for(int i = 0; i < numButtons; i++){
+		if(states[i]){
+			Input::btnRelease(i);
+		}else{
+			Input::btnPress(i);
+		}
+	}
+}
+
---
+++ src/Input/InputShift.h
@@ -0,0 +1,24 @@
+#ifndef CIRCUITOS_INPUTSHIFT_H
+#define CIRCUITOS_INPUTSHIFT_H
+
+#include "Input.h"
+
+class InputShift : public Input {
+public:
+	InputShift(uint8_t dataPin, uint8_t clockPin, uint8_t loadPin, uint8_t numButtons);
+	void begin();
+
+protected:
+	void scanButtons() override;
+
+private:
+	uint8_t dataPin;
+	uint8_t clockPin;
+	uint8_t loadPin;
+
+	uint8_t numButtons;
+	uint8_t numShifts;
+
+};
+
+#endif //CIRCUITOS_INPUTSHIFT_H
