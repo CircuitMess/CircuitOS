cc327f40667c04e79965940463269b5e1e383a0a
Filip Budiša <filip@circuitmess.com>
2022-03-13 15:51:23 +0100

    Matrix refactor

--- src/Devices/Matrix/Matrix.cpp
+++ src/Devices/Matrix/Matrix.cpp
@@ -1,99 +1,55 @@
 #include "Matrix.h"
 #include "Font.hpp"
 
-#ifndef _swap_int16_t
-#define _swap_int16_t(a, b)                                                    \
-{                                                                            \
-    int16_t t = a;                                                             \
-    a = b;                                                                     \
-    b = t;                                                                     \
-}
-#endif
+Matrix::Matrix(MatrixOutput& output) : output(output), width(output.getWidth()), height(output.getHeight()), data(output.getWidth(), output.getHeight()){
 
-/**************************************************************************/
-/*!
-	@brief Constructor for breakout version.
-	@param width Desired width of led display.
-	@param height Desired height of led display.
-*/
-/**************************************************************************/
-
-Matrix::Matrix(MatrixOutput &output, uint8_t _width, uint8_t _height) : output(output){
-	width = _width;
-	height = _height;
-	matrixBuffer = (uint8_t*)calloc(width * height, sizeof(uint8_t));
-	pastMatrixBuffer = (uint8_t*)calloc(width * height, sizeof(uint8_t));
 }
 
-/**************************************************************************/
-/*!
-	@brief Initialize hardware and clear display.
-*/
-/**************************************************************************/
 void Matrix::begin(){
-	output.init();
-	clear(); // set each led to 0 PWM
-	push();
 	clear();
+	push();
 }
 
-void Matrix::setOutput(MatrixOutput &output){
-	Matrix::output = output;
-}
-
-/**************************************************************************/
-/*!
-	@brief Sets all LEDs on & 0 PWM for current frame.
-*/
-/**************************************************************************/
 void Matrix::clear(){
-	memset(matrixBuffer, 0, width * height);
+	data.clear();
 }
 
-/**************************************************************************/
-/*!
-	@brief Low level accesssor - sets a 8-bit PWM pixel value
-	handles rotation and pixel arrangement, unlike setLEDPWM
-	@param x The x position, starting with 0 for left-most side
-	@param y The y position, starting with 0 for top-most side
-	@param color Despite being a 16-bit value, takes 0 (off) to 255 (max on)
-*/
-/**************************************************************************/
-void Matrix::drawPixel(int16_t x, int16_t y, uint16_t color){
-	switch(getRotation()){
-		case 1:_swap_int16_t(x, y);
+void Matrix::push(){
+	output.push(data);
+}
+
+void Matrix::drawPixel(uint16_t x, uint16_t y, const MatrixPixel& color){
+	switch(rotation){
+		case 1:
+			std::swap(x, y);
 			x = width - x - 1;
 			break;
 		case 2:
 			x = width - x - 1;
 			y = height - y - 1;
 			break;
-		case 3:_swap_int16_t(x, y);
+		case 3:
+			std::swap(x, y);
 			y = height - y - 1;
 			break;
 	}
 
-	if((x < 0) || (x >= 16))
+	if((x < 0) || (x >= width))
 		return;
-	if((y < 0) || (y >= 9))
+	if((y < 0) || (y >= height))
 		return;
-	if(color > 255)
-		color = 255; // PWM 8bit max
-	matrixBuffer[x + y * 16] = color;
+
+	data[x][y] = color;
 }
 
-/**************************************************************************/
-/*!
-	@brief  Draw a single character on the desired position. Uses a standard ASCII 5x7 font.
-	@param   x The x position, starting with 0 for left-most side.
-	@param   y The y position, starting with 0 for top-most side
-	@param   c The character to be displayed.
-	@param   _brightness Brightness of the character. Affected by the global setBrightness setting.
-	@param   bigFont Flag to use bigger 5x7 or the smaller 3x5 font: true - big font, false - small font.
-*/
-/**************************************************************************/
-void Matrix::drawChar(int32_t x, int32_t y, unsigned char c, uint8_t _brightness, bool bigFont){
-	if(bigFont){
+void Matrix::drawPixel(uint16_t i, const MatrixPixel& color){
+	uint16_t y = i / width;
+	uint16_t x = i - y * width;
+	drawPixel(x, y, color);
+}
+
+void Matrix::drawChar(int32_t x, int32_t y, unsigned char c, const MatrixPixel& color){
+	if(font == BIG){
 		uint8_t column[6];
 		uint8_t mask = 0x1;
 
@@ -105,14 +61,13 @@ void Matrix::drawChar(int32_t x, int32_t y, unsigned char c, uint8_t _brightness
 			for(k = 0; k < 5; k++){
 				if(column[k] & mask){
 					if((x + k) >= 0 && (y + j) >= 0 && (x + k) < width && (y + j) < height){
-						drawPixel(x + k, y + j, _brightness);
+						drawPixel(x + k, y + j, color);
 					}
 				}
 			}
 			mask <<= 1;
 		}
 	}else{
-
 		if((c >= TomThumb.first) && (c <= TomThumb.last)){
 			c -= TomThumb.first;
 			GFXglyph* glyph = &(TomThumb.glyph[c]);
@@ -132,7 +87,7 @@ void Matrix::drawChar(int32_t x, int32_t y, unsigned char c, uint8_t _brightness
 					}
 					if(bits & 0x80){
 						if((x + xo + xx) >= 0 && (y + yo + yy) >= 0 && (x + xo + xx) < width && (y + yo + yy) < height){
-							drawPixel((x + xo + xx) % 16, y + yo + yy, _brightness);
+							drawPixel((x + xo + xx) % 16, y + yo + yy, color);
 						}
 					}
 					bits <<= 1;
@@ -142,200 +97,67 @@ void Matrix::drawChar(int32_t x, int32_t y, unsigned char c, uint8_t _brightness
 	}
 }
 
-/**************************************************************************/
-/*!
-	@brief  Draws a character array on the desired position. Uses a standard ASCII 5x7 font.
-	@param   x The x position, starting with 0 for left-most side.
-	@param   y The y position, starting with 0 for top-most side
-	@param   c The character array to be displayed.
-	@param   _brightness Brightness of the character. Affected by the global setBrightness setting.
-	@param   bigFont Flag to use bigger 5x7 or the smaller 3x5 font: true - big font, false - small font.
-*/
-/**************************************************************************/
-void Matrix::drawString(int32_t x, int32_t y, const char* c, uint8_t _brightness, bool bigFont){
-	size_t length = strlen(c);
-	for(size_t i = 0; i < length; i++){
-		drawChar(x, y, c[i], _brightness, bigFont);
-		x += bigFont ? 6 : 4;
+void Matrix::drawString(int32_t x, int32_t y, const String &s, const MatrixPixel& color){
+	for(size_t i = 0; i < s.length(); i++){
+		drawChar(x, y, s[i], color);
+		x += font == BIG ? 6 : 4;
 	}
 }
 
-void Matrix::drawString(int32_t x, int32_t y, const String &s, uint8_t _brightness, bool bigFont){
-	drawString(x, y, s.c_str(), _brightness, bigFont);
+void Matrix::drawBitmap(uint16_t xPos, uint16_t yPos, uint16_t w, uint16_t h, const uint8_t* data, const MatrixPixel& color){
+	MatrixPixel c = color;
+
+	for(int x = 0; x < w; x++){
+		for(int y = 0; y < h; y++){
+			c.i = data[y * w + x];
+			drawPixel(xPos + x, yPos + y, c);
+		}
+	}
 }
 
-/**************************************************************************/
-/*!
-	@brief  Sets global brightness for the matrix.
-	@param   _brightness Global brightness for the matrix. Ranges from 0 to 255.
-*/
-/**************************************************************************/
+void Matrix::drawBitmap(uint16_t xPos, uint16_t yPos, const MatrixPixelData& data){
+	for(int x = 0; x < data.getWidth(); x++){
+		for(int y = 0; y < data.getHeight(); y++){
+			drawPixel(xPos + x, yPos + y, data.get(x, y));
+		}
+	}
+}
+
+MatrixAnimation& Matrix::startAnimation(fs::File file){
+	animation = MatrixAnimation(this, file);
+	animation.start();
+	return animation;
+}
+
+MatrixAnimation& Matrix::getAnimation(){
+	return animation;
+}
+
+void Matrix::stopAnimation(){
+	animation = { };
+}
+
 void Matrix::setBrightness(uint8_t _brightness){
 	output.setBrightness(_brightness);
 }
 
-/**************************************************************************/
-/*!
-	@brief  Gets global brightness for the matrix.
-	@return   Global brightness of the matrix.
-*/
-/**************************************************************************/
 uint8_t Matrix::getBrightness() const{
 	return output.getBrightness();
 }
 
-/**************************************************************************/
-/*!
-	@brief  Sets matrix rotation.
-	@param   rot Desired rotation in clockwise direction (0-3).
-*/
-/**************************************************************************/
-void Matrix::setRotation(uint8_t rot){
+/*void Matrix::setRotation(uint8_t rot){
 	if(rot > 3) return;
 	rotation = rot;
 }
 
-/**************************************************************************/
-/*!
-	@brief  Gets matrix rotation.
-	@return   Matrix rotation (0-3).
-*/
-/**************************************************************************/
 uint8_t Matrix::getRotation() const{
 	return rotation;
+}*/
+
+Matrix::Font Matrix::getFont() const{
+	return font;
 }
 
-/**************************************************************************/
-/*!
-	@brief  Push the matrix buffer onto the matrix.
-*/
-/**************************************************************************/
-void Matrix::push(){
-	output.push(matrixBuffer);
-}
-
-/**************************************************************************/
-/*!
-	@brief  Starts animation. Frames will be updated inside updateAnimation().
-	@param  animation Pointer to desired animation.
-	@param  loop Sets animation loop. True - loop until stopAnimation() or another startAnimation(), False - no looping.
-*/
-/**************************************************************************/
-void Matrix::startAnimation(Animation* _animation, bool loop){
-	stopAnimation();
-
-	animation = _animation;
-	animationLoop = loop;
-	animationFrame = animation->getNextFrame();
-	drawBitmap(0, 0, animation->getWidth(), animation->getHeight(), animationFrame->data);
-	push();
-	currentFrameTime = 0;
-	animationStartMicros = micros();
-}
-
-/**************************************************************************/
-/*!
-	@brief  Stops running animation.
-*/
-/**************************************************************************/
-void Matrix::stopAnimation(){
-	delete animation;
-	animation = nullptr;
-	animationFrame = nullptr;
-	currentFrameTime = 0;
-}
-
-/**************************************************************************/
-/*!
-	@brief  Updates running animation. Expected to be used inside loop().
-	@param  _time Current millis() time, used for frame duration calculation.
-*/
-/**************************************************************************/
-void Matrix::loop(uint _time){
-	if(animationFrame != nullptr && animation != nullptr){
-		currentFrameTime += _time;
-		if(currentFrameTime >= animationFrame->duration * 1000){
-			clear();
-			currentFrameTime = 0;
-			animationFrame = animation->getNextFrame();
-			if(animationFrame == nullptr){
-				if(animationLoop){
-					animationStartMicros = micros();
-					animation->rewind();
-					animationFrame = animation->getNextFrame();
-				}else{
-					stopAnimation();
-					return;
-				}
-			}
-			drawBitmap(0, 0, animation->getWidth(), animation->getHeight(), animationFrame->data);
-		}
-	}
-	bool noChange = true;
-	if(prevBrightness == output.getBrightness()){
-		for(uint8_t i = 0; i < width * height; i++){
-			if(matrixBuffer[i] != pastMatrixBuffer[i]){
-				noChange = false;
-				break;
-			}
-		}
-	}else{
-		noChange = false;
-		prevBrightness = output.getBrightness();
-	}
-	if(!noChange){
-		push();
-	}
-	memcpy(pastMatrixBuffer, matrixBuffer, width * height);
-}
-
-/**************************************************************************/
-/*!
-	@brief  Draws a bitmap on the desired position. Expects an 8-bit monochrome array.
-	@param   x The x position, starting with 0 for left-most side.
-	@param   y The y position, starting with 0 for top-most side.
-	@param   width Bitmap width.
-	@param   height Bitmap height.
-	@param   data Pointer to the bitmap.
-*/
-/**************************************************************************/
-void Matrix::drawBitmap(int x, int y, uint width, uint height, uint8_t* data){
-	for(int i = 0; i < height; i++){
-		for(int j = 0; j < width; j++){
-			drawPixel(x + j, y + i, data[i * width + j]);
-		}
-	}
-}
-
-/**************************************************************************/
-/*!
-	@brief  Draws a bitmap on the desired position. Expects a 24-bit color array, uses only the red color value.
-	@param   x The x position, starting with 0 for left-most side.
-	@param   y The y position, starting with 0 for top-most side.
-	@param   width Bitmap width.
-	@param   height Bitmap height.
-	@param   data Pointer to the bitmap.
-*/
-/**************************************************************************/
-void Matrix::drawBitmap(int x, int y, uint width, uint height, RGBpixel* data){
-	for(int i = 0; i < height; i++){
-		for(int j = 0; j < width; j++){
-			drawPixel(x + j, y + i, data[i * width + j].r);
-		}
-	}
-}
-
-/**************************************************************************/
-/*!
-	@brief  Returns the completion rate in percentage for the current animation.
-	@return  Completion rate (in percentage 0-100) for the current animation. If none are played, then defaults to zero.
-*/
-/**************************************************************************/
-float Matrix::getAnimationCompletionRate(){
-	if(animationFrame == nullptr || animation == nullptr) return 0.0;
-	return ((float)(micros() - animationStartMicros)) / ((float)(animation->getLoopDuration() * 1000)) * 100;
-}
-
-void Matrix::drawPixel(uint8_t i, uint8_t brightness){
-	matrixBuffer[i] = brightness;
+void Matrix::setFont(Matrix::Font font){
+	Matrix::font = font;
 }
--- src/Devices/Matrix/Matrix.h
+++ src/Devices/Matrix/Matrix.h
@@ -2,47 +2,57 @@
 #define SPENCER_LEDMATRIX_H
 
 #include <Arduino.h>
-#include "Animation.h"
-#include <Loop/LoopListener.h>
 #include "MatrixOutput.h"
+#include "MatrixAnimation.h"
 
-class Matrix : public LoopListener {
+class Matrix {
 public:
-	Matrix(MatrixOutput &output, uint8_t width = 16, uint8_t height = 9);
-	void setOutput(MatrixOutput &output);
+	Matrix(MatrixOutput& output);
 	void begin();
-	void drawPixel(int16_t x, int16_t y, uint16_t color);
-	void drawPixel(uint8_t i, uint8_t brightness);
 	void clear();
-
-	void setRotation(uint8_t rotation);
-	uint8_t getRotation() const;
-	void drawChar(int32_t x, int32_t y, unsigned char c, uint8_t brightness = 255, bool bigFont = true);
-	void drawString(int32_t x, int32_t y, const char* c, uint8_t brightness = 255, bool bigFont = true);
-	void drawString(int32_t x, int32_t y, const String &s, uint8_t brightness = 255, bool bigFont = true);
-	void setBrightness(uint8_t _brightness);
-	uint8_t getBrightness() const;
 	void push();
-	void drawBitmap(int x, int y, uint width, uint height, uint8_t* data);
-	void drawBitmap(int x, int y, uint width, uint height, RGBpixel* data);
 
-	void startAnimation(Animation* _animation, bool loop);
-	float getAnimationCompletionRate();
+	/*void setRotation(uint8_t rotation);
+	uint8_t getRotation() const;*/
+
+	/**
+	 * Set global brightness.
+	 * @param brightness 0-255
+	 */
+	void setBrightness(uint8_t brightness);
+	uint8_t getBrightness() const;
+
+	enum Font { BIG, SMALL };
+
+	/**
+	 * Set the drawChar and drawString font. BIG is 5x7, SMALL is 3x5
+	 * @param font BIG or SMALL
+	 */
+	void setFont(Font font);
+	Font getFont() const;
+
+	void drawPixel(uint16_t x, uint16_t y, const MatrixPixel& color);
+	void drawPixel(uint16_t i, const MatrixPixel& color);
+
+	void drawChar(int32_t x, int32_t y, unsigned char c, const MatrixPixel& color = { 255, 255, 255, 255 });
+	void drawString(int32_t x, int32_t y, const String &text, const MatrixPixel& color = { 255, 255, 255, 255 });
+
+	void drawBitmap(uint16_t x, uint16_t y, uint16_t width, uint16_t height, const uint8_t* data, const MatrixPixel& color = { 255, 255, 255, 255 });
+	void drawBitmap(uint16_t x, uint16_t y, const MatrixPixelData& data);
+
+	MatrixAnimation& startAnimation(fs::File file);
+	MatrixAnimation& getAnimation();
 	void stopAnimation();
-	void loop(uint _time) override;
 
 private:
-	MatrixOutput &output;
+	MatrixOutput& output;
+	MatrixPixelData data;
 
-	uint8_t width, height, rotation = 2, prevBrightness = 0;
-	uint8_t* matrixBuffer = nullptr;
-	uint8_t* pastMatrixBuffer = nullptr;
+	uint8_t width, height, rotation = 0;
+	Font font = BIG;
 
-	AnimationFrame* animationFrame = nullptr;
-	Animation* animation = nullptr;
-	bool animationLoop = false;
-	uint currentFrameTime = 0;
-	uint animationStartMicros = 0;
+	MatrixAnimation animation;
+
 };
 
 #endif
