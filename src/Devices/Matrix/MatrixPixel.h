c5ee64fb1a6281bce9363abef4e151048366d3a4
Filip Budiša <filip@circuitmess.com>
2022-03-13 15:48:37 +0100

    Add MatrixPixel and MatrixPixelData structures

---
+++ src/Devices/Matrix/MatrixPixel.h
@@ -0,0 +1,45 @@
+#ifndef CIRCUITOS_MATRIXPIXEL_H
+#define CIRCUITOS_MATRIXPIXEL_H
+
+#include <Arduino.h>
+#include <vector>
+#include "../../Util/GIF.h"
+
+struct MatrixPixel {
+	uint8_t r, g, b, i;
+};
+
+class MatrixPixelData {
+public:
+	MatrixPixelData(uint16_t width, uint16_t height);
+	MatrixPixelData(const GIF::Frame& gifFrame);
+	MatrixPixelData(const MatrixPixelData& other);
+	MatrixPixelData& operator=(const MatrixPixelData& other);
+
+	uint16_t getWidth() const;
+	uint16_t getHeight() const;
+
+	void set(uint16_t x, uint16_t y, const MatrixPixel& pixel);
+	MatrixPixel get(uint16_t x, uint16_t y) const;
+
+	void clear(const MatrixPixel& color = { 0, 0, 0, 0 });
+
+	class Column {
+	public:
+		Column(MatrixPixelData& parent, uint16_t x);
+		MatrixPixel& operator[](uint16_t y) const;
+
+	private:
+		MatrixPixelData& parent;
+		uint16_t x;
+	};
+
+	Column operator[](uint16_t x);
+
+private:
+	uint16_t width, height;
+	std::vector<std::vector<MatrixPixel>> data;
+
+};
+
+#endif //CIRCUITOS_MATRIXPIXEL_H
