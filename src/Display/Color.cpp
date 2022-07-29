e6efe721ff51423f71261a8f48e030c9a058cddf
Emil Gajšak <emil.gajsak@gmail.com>
2022-07-29 12:00:45 +0200

    Added Pixel color presets in Display/Color.h

--- src/Display/Color.cpp
+++ src/Display/Color.cpp
@@ -1,5 +1,14 @@
 #include "Color.h"
 
+const Pixel Pixel::Red = { 255, 0, 0};
+const Pixel Pixel::Green = { 0, 255, 0};
+const Pixel Pixel::Blue = { 0, 0, 255};
+const Pixel Pixel::Yellow = { 255, 255, 0};
+const Pixel Pixel::Cyan = { 0, 255, 255};
+const Pixel Pixel::Magenta = { 255, 0, 255};
+const Pixel Pixel::White = { 255, 255, 255};
+const Pixel Pixel::Black = { 0, 0, 0};
+
 Color C_RGB(uint8_t r, uint8_t g, uint8_t b){
 	return (((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3));
 }
--- src/Display/Color.h
+++ src/Display/Color.h
@@ -5,7 +5,16 @@
 
 typedef uint16_t Color;
 
-struct Pixel { uint8_t r, g, b; };
+struct Pixel { uint8_t r, g, b;
+	static const Pixel Red;
+	static const Pixel Green;
+	static const Pixel Blue;
+	static const Pixel Magenta;
+	static const Pixel Cyan;
+	static const Pixel Yellow;
+	static const Pixel White;
+	static const Pixel Black;
+};
 
 Color C_RGB(uint8_t r, uint8_t g, uint8_t b);
 
